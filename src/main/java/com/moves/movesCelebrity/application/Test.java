1f5c4577e5f348b544e3f7c4d24c552469be296a
Kalidoss <kalidosss.dev@auxolabs.in>
2018-09-21 13:42:37 +0530

    Test class added for github testing

--- src/main/java/com/moves/movesCelebrity/application/Test.java
+++
@@ -1,7 +0,0 @@
-package com.moves.movesCelebrity.application;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("testing");
-    }
-}
--- src/main/java/com/moves/movesCelebrity/application/Test1.java
+++
@@ -1,7 +0,0 @@
-package com.moves.movesCelebrity.application;
-
-public class Test1 {
-    public static void main(String[] args) {
-        System.out.println("testing 2");
-    }
-}
